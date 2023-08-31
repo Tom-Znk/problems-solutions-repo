521dd7e8416878596f2c03e0229c35d9424230bf
Tom-Znk <tomasz@unifysquare.com>
2023-08-31 22:04:51 +0300

    added link to 1326

--- leetcode/1326/solution.py
+++ leetcode/1326/solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
 
-        """Basically a DP approach"""
+        """
+        Basically a standard DP approach
+        LINK : https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/description/
+        """
 
 
         tap_cover = [float('inf')] * (n + 1)
         tap_cover[0] = 0
         
-        for i, r in enumerate(ranges):
+        for i, tap_diameter in enumerate(ranges):
 
-            start, end = max(0, i - r), min(n, i + r)
+            start, end = max(0, i - tap_diameter), min(n, i + tap_diameter)
 
             for j in range(start + 1, end + 1):
                 
